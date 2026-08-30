4fe8f3abba15bda9fd46553ce4f1a70b50012824
Vaibhav Shukla <151714466+Necromancervbh@users.noreply.github.com>
2026-08-30 21:47:27 +0530

    chore: organize files into projects directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Nexus Core - Primary Library Entrypoint
- * High-performance developer utilities, data science routines, and backend helpers.
- */
-
-export * from './utils/toolkit.js';
-export * from './data-science/time-series/anomalyDetector.js';
