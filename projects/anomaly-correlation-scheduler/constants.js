ece5505d34b2187e9a21f8f613f83b5e2f04cf8c
Vaibhav Shukla <151714466+Necromancervbh@users.noreply.github.com>
2026-09-14 12:49:39 +0530

    chore(projects/anomaly-correlation-scheduler): define default constants and algorithm hyperparameters

---
+++ projects/anomaly-correlation-scheduler/constants.js
@@ -0,0 +1,6 @@
+/**
+ * Hyperparameters & Default Constants for Anomaly Correlation Forecast Scheduler
+ */
+export const DEFAULT_TIMEOUT_MS = 5000;
+export const MAX_RETRY_COUNT = 3;
+export const EPSILON = 1e-9;
